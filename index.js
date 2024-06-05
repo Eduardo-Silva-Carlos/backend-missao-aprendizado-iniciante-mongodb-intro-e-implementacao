98531ebe14346082d57d3c927f7e4ee2926417e1
Carlos Eduardo <121635641+Eduardo-Silva-Carlos@users.noreply.github.com>
2024-06-05 05:52:02 +0000

    Banco de dado implementado no EndPoint 'Update'

--- index.js
+++ index.js
@@ -26,9 +26,9 @@ async function main() {
 
   // EndPoint Read All '[GET] /personagem'
   app.get('/personagem', async function (req, res) {
-  // Acessamos a lista de intens na collection do mongoDB
+    // Acessamos a lista de intens na collection do mongoDB
     const itens = await collection.find().toArray()
-  // Enviamos a lista de intens como resultado
+    // Enviamos a lista de intens como resultado
     res.send(itens)
   })
 
@@ -38,7 +38,7 @@ async function main() {
     const id = req.params.id
 
     // Acessa o item na collection usando o ID
-    const item = await collection.findOne({_id:new ObjectId(id)})
+    const item = await collection.findOne({ _id: new ObjectId(id) })
 
     //Checamos se o item foi obtido é existente 
     if (!item) {
@@ -54,7 +54,7 @@ async function main() {
 
   // EndPoint Create [POST] /personagem
   app.post('/personagem', async function (req, res) {
-    
+
     //Acessamos o bory da requisição
     const novoItem = req.body
 
@@ -78,34 +78,34 @@ async function main() {
   })
 
   //EndPoint Update [PUT]/personagem/:id 
-  app.put('/personagem/:id', function (req, res) {
+  app.put('/personagem/:id', async function (req, res) {
     //Acessamos o ID dos parâmetros da rota
     const id = req.params.id
 
-    //Checamos se o item do id-1 está na lista , exibindo um mensagem caso não esteja
-    if (!lista[id - 1]) {
-      return res.status(404).send('Item não encontrado!')
-    }
+    // //Checamos se o item do id-1 está na lista , exibindo um mensagem caso não esteja
+    // if (!lista[id - 1]) {
+    //   return res.status(404).send('Item não encontrado!')
+    // }
 
     // Acessamos o Bory de requisição 
-    const bory = req.body
-
-    // Acessamos a propriedade nome do Bory
-    const novoItem = bory.nome
+    const novoItem = req.body
 
     //Checa se o 'nome' esta presente no body
-    if (!novoItem) {
+    if (!novoItem || !novoItem.nome) {
       return res.status(400).send('Corpo da requisição conter a propriedade `nome`.')
     }
-    // Checa se o novoItem está na lista ou não
-    if (lista.includes(novoItem)) {
-      return res.status(409).send('Esse item já existe na lista!')
-    }
-    // Atualizamos na lista o novoItem pelo Id - 1 
-    lista[id - 1] = novoItem
+    // // Checa se o novoItem está na lista ou não
+    // if (lista.includes(novoItem)) {
+    //   return res.status(409).send('Esse item já existe na lista!')
+    // }
+    // Atualizamos na collection  novoItem pelo ID
+    await collection.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: novoItem }
+    )
 
     // Eviamos uma mensagem de sucesso 
-    res.send('Item atualizado com sucesso : ' + id + '-' + novoItem)
+    res.send(novoItem)
 
   })
   // EndPoint delete [DELETE]/personagem/:id 
